0777d1e6eaf53ffc77432d9408adad7bec2b77db
SavioFagundes <AkiraSfM@gmail.com>
2025-05-19 19:03:33 -0300

    Initial Commit

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+namespace Dominio.DTOs
+{
+    public class loginDto
+    {
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "aaaaaaaaa");
+
+app.MapPost("/login", (loginDto loginDto) => {
+    if (loginDto.Email == "adm@teste.com" && loginDto.Password == "123456")
+    {
+        return Results.Ok(new { message = "Login realizado com sucesso" });
+    }
+    return Results.Unauthorized();
+});
+
+app.Run();
+
+public class loginDto
+{
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+}
